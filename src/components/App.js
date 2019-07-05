b9bfa30dcc8514551da5a7ae72f203db25305974
dinahamamo <dina.hamamo@gmail.com>
2019-07-05 18:18:50 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Players from "./Players";
+import Teams from "./Teams";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/players" component={Players} />
+          <Route path="/teams" component={Teams} />
+          <Route
+            render={() => <h1 className="text-center">Four oh Four.</h1>}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
