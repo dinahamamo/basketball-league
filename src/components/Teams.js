fb876d0545d2d477398bf3f1b2363b4de9710f77
dinahamamo <dina.hamamo@gmail.com>
2019-07-09 16:24:32 +0200

    Fetch the teams

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import { getTeamNames } from "../api";
 
-function Teams() {
-  return <div className="container two-column">Teams</div>;
+class Teams extends Component {
+  state = {
+    teamNames: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    getTeamNames().then(teamNames =>
+      this.setState({ teamNames, loading: false })
+    );
+  }
+
+  render() {
+    const { loading, teamNames } = this.state;
+    const { location, match } = this.props;
+
+    return (
+      <div className="container two-column">
+        <Sidebar
+          loading={loading}
+          title="Teams"
+          list={teamNames}
+          {...this.props}
+        />
+        {loading === false && location.pathname === "/teams" ? (
+          <div className="sidebar-instruction">Select a Team</div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default Teams;
